34a937797abadeac069f91a11c1c5391f8bd32f8
Josiel Borges <josielborges555@gmail.com>
2023-03-21 11:15:49 -0300

    feat: new colors

--- src/styles/Global.tsx
+++ src/styles/Global.tsx
@@ -81,11 +81,10 @@ export const globalStyles = globalCss({
 });
 
 export const GlobalStyle = ({ children }: GlobalStyleProps): JSX.Element => {
-
   const theme = createTheme({
     colors: {
-      brand1: "#623CEA",
-      brand2: "#311E75",
+      brand1: "#ebc41a",
+      brand2: "#a7741c",
 
       grey0: "#0A0A0B",
       grey1: "#121214",
@@ -95,7 +94,7 @@ export const GlobalStyle = ({ children }: GlobalStyleProps): JSX.Element => {
       grey5: "#F8F9FA",
 
       socialInstagram: "#CF50AC",
-      socialFacebook: "#506CCF",
+      socialGithub: "#ffffff",
       socialLinkedin: "#0E76A8",
 
       whiteFixed: "#ffffff",
